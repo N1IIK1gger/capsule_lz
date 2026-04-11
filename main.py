f60fce9585172520800813ffdfbce90e395fec7b
N1IIK1gger <5z6jrkmvh8@privaterelay.appleid.com>
2026-04-11 03:24:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-import log as logging
+from datetime import datetime
+import pandas as pd
+import os
 
-def main():
 
-    logging.main()
 
-if __name__=="__main__":
-    main()
+def func_lgn(func):
+    def LGNM(*args, **kwargs):
+        usr_func = func
+        org = func(*args, **kwargs)
+        usr_func_name = str(usr_func.__name__)
+        usr_name = os.getlogin()
+        time_act = str(datetime.now().time())
+        day_act =  str(datetime.now().date())
+        logs = 'logs.csv'
+        
+        if os.path.isfile(logs):
+            file_df = pd.read_csv(logs)
+            data = {'': [len(file_df)], 'User': [usr_name], 'Func': [usr_func_name], 'Time':[time_act], 'Date':[day_act]}
+            df = pd.DataFrame(data)
+            df.to_csv('logs.csv',header=False, index=False, mode='a')
+            
+        else:
+            data = {'User': [usr_name], 'Func': [usr_func_name], 'Time':[time_act], 'Date':[day_act]}
+            df = pd.DataFrame(data)
+            df.to_csv('logs.csv')
+        return org
+    
+    return LGNM
+
+@func_lgn
+def pr():
+    print('ВАШЕ ЛОГИРОВАНИЕ ПРОШЛО УСПЕШНО')
+pr()
